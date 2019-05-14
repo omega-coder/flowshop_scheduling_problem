a4c5f058628ebd82f2d640c6d6daa687742ee749
omega-coder <fy_cherief@esi.dz>
2019-05-14 23:56:24 +0200

    Adds create plot function to create a bar graph for testing

--- app.py
+++ app.py
@@ -2,14 +2,40 @@
 
 import flask
 from flask import Flask, render_template, url_for, abort
+import plotly
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import json
+
+
+
+
+def create_plot():
+    n = 30
+    x = np.linspace(0, 1, n)
+    y = np.random.randn(n)
+    df = pd.DataFrame({'x': x, 'y': y})
+
+    data = [
+        go.Bar(
+            x = df["x"],
+            y = df["y"]
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index')
+    bar = create_plot()
+    return render_template('index.html', plot=bar)
+
+
 
 
 if __name__ == "__main__":
