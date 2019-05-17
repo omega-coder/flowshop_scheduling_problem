f612afceceafedb49a64593d399fdffebf15995d
omega-coder <fy_cherief@esi.dz>
2019-05-18 00:47:57 +0200

    Adds code for AJAX call to /solve route

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+    $("#solve").on('click', function() {
+        $.ajax({
+            url: '/solve',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify('algorithm', $('#algorithm').val(), 'data': $('#data').val());
+            processData: false,
+            success: function(data, textStatus, jQxhr) {
+                // REPLOT the gentt chart, according to repsonse
+            },
+            error: function(jQxhr, textStatus, errorThrow) {
+                // MAKE ERROR Appear
+            }
+        });    
+    });
+}); 
+
 
 
 
