fe8172c3b207887fadf508decd7986e3dc152fcc
omega-coder <fy_cherief@esi.dz>
2019-05-20 04:21:55 +0200

    Replaces Hardcoded problem on main route with a random problem

--- app.py
+++ app.py
@@ -100,64 +100,8 @@ def solve():
 
 @app.route('/')
 def index():
-    nb_m = 2
-    nb_j = 6
-    data = [[6, 2, 10, 4, 5, 3], [5, 4, 3, 8, 2, 4]]
-    pfsp = Flowshop(data, nb_m, nb_j)
-    _, jobs_m1, jobs_m2 = pfsp.solve_johnson()
-
-    df = []
-    curr_date = datetime.datetime.now()
-    for i, j in zip(jobs_m1, jobs_m2):
-        Start = (
-            datetime.timedelta(
-                seconds=i["start_time"],
-            ) + curr_date
-        ).strftime(
-            "%Y-%m-%d %H:%M:%S",
-        )
-        Finish = (
-            datetime.timedelta(
-                seconds=i["end_time"],
-            ) + curr_date
-        ).strftime(
-            "%Y-%m-%d %H:%M:%S",
-        )
-        task = {"Task": "M1",
-                "Start": Start,
-                "Finish": Finish,
-                "Resource": i["name"]}
-        df.append(task)
-        Start = (
-            datetime.timedelta(
-                seconds=j["start_time"],
-            ) + curr_date
-        ).strftime(
-            "%Y-%m-%d %H:%M:%S",
-        )
-        Finish = (
-            datetime.timedelta(
-                seconds=j["end_time"],
-            ) + curr_date
-        ).strftime(
-            "%Y-%m-%d %H:%M:%S",
-        )
-        task = {"Task": "M2",
-                "Start": Start,
-                "Finish": Finish,
-                "Resource": j["name"]}
-        df.append(task)
-    fig = ff.create_gantt(
-        df,
-        group_tasks=True,
-        index_col="Resource",
-        show_colorbar=True,
-        showgrid_x=True,
-        showgrid_y=True,
-        bar_width=0.1,
-    )
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('index.html', plot=graphJSON)
+    graph_json = random_johnson(2, 6)
+    return render_template('index.html', plot=graph_json)
 
 
 if __name__ == "__main__":
