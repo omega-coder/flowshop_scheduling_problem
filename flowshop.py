ee5b6c602acadd7bed40078b7b44864bf2276096
omega-coder <fy_cherief@esi.dz>
2019-05-21 05:12:19 +0200

    Documents code and changes return type of solve_johnson method

--- flowshop.py
+++ flowshop.py
@@ -5,13 +5,35 @@ import numpy as np
 
 
 class Flowshop(object):
+    """
+    A class for initiaizing & solving a Permutation Flowshop Scheduling Problem
+    """
 
-    def __init__(self, data=None, nb_machines=2, nb_jobs=None):
-        self.data = data
+    def __init__(self, data=None, nb_machines=2, nb_jobs=6):
+        """[summary]
+
+        Keyword Arguments:
+            data {list} -- A 2D array of processing time on machines (default: {None})
+            nb_machines {int} -- Number of machines for the given problem must be the number of rows of first param (default: {2})
+            nb_jobs {[type]} -- Number of jobs for the given problem, must be equal to the number of columns of the data param. (default: {6})
+        """
         self.nb_machines = nb_machines
         self.nb_jobs = nb_jobs
+        if data is not None:
+            self.data = data
+        else:
+            self.data = RandomFlowshop(
+                self.nb_machines, self.nb_jobs).get_data()
 
     def solve_johnson(self):
+        """Solves a permutation flowshop problem using johnson's rule for a permutation problem of 2 machines and N jobs 
+
+        Raises:
+            Exception: Raises exception when given a problem with more than 2 machines
+
+        Returns:
+            tuple -- returns a tuple containing the optimal sequence of scheduled jobs and a list of scheduled jobs on the 2 machines
+        """
         if self.nb_machines != 2:
             raise Exception(
                 "Johnson's algorithm only possible for a 2 machine problem"
@@ -25,8 +47,7 @@ class Flowshop(object):
         machine_2_sequence.sort(key=lambda x: self.data[1][x], reverse=True)
 
         seq = machine_1_sequence + machine_2_sequence
-        jobs_m1 = []
-        jobs_m2 = []
+        jobs_m1, jobs_m2 = [], []
         job_name_rad = "job_"
         job = {"name": job_name_rad +
                str(
@@ -57,16 +78,16 @@ class Flowshop(object):
                    "end_time": job_end_m2}
             jobs_m2.append(job)
 
-        return seq, jobs_m1, jobs_m2
+        return seq, [jobs_m1, jobs_m2]
 
 
 class RandomFlowshop:
-    """
-     This module makes an instance of random flowshop problem,
+    """This module makes an instance of random flowshop problem,
      given number of machines and number of jobs,
      and generates random data of jobs processing times
-     Example:
-        random_problem = RandomFlowshop(2, 7)
+
+    Returns:
+        RandomFlowshop object -- A RandomFlowshop object instance
     """
 
     def __init__(self, nb_machines, nb_jobs):
@@ -130,5 +151,7 @@ class RandomFlowshop:
 
 if __name__ == "__main__":
     random_problem = RandomFlowshop(2, 6)
-    pfsp = Flowshop(random_problem.get_data(), 2, 6)
-    print(pfsp)
+    random_problem_instance = random_problem.get_problem_instance()
+    seq, jobs = random_problem_instance.solve_johnson()
+    print("Sequence: {} \nJobs on Machine 1: \n {} \n Jobs on machine 2:\n {} \n".format(
+        seq, jobs[0], jobs[1]))
