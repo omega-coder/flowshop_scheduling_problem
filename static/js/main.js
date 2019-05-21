b73eac7c90cbeb7994596d8d8e7bccb5c57ee4d4
omega-coder <fy_cherief@esi.dz>
2019-05-21 09:40:21 +0200

    Adds event handlers for controlling number of machines

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
-    $("#solve").on('click', function() {
-        $.ajax({
-            url: '/solve',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify( { 'algorithm': $('#algorithm').val(), 'data': $('#data').val() }),
-            processData: false,
-            success: function(data, textStatus, jQxhr) {
-                Plotly.newPlot('gantt', data, {});
-            },
-            error: function(jQxhr, textStatus, errorThrow) {
-                console.log(textStatus);
-            }
-        });    
+  $("#nb_machines").change(function() {
+    if ($("#algorithm").val() === "johnson") {
+      if ($("#nb_machines") !== 2) {
+        $("#nb_machines").val(2);
+      }
+    } else {
+      if ($(this).val() <= 2) {
+        $("#nb_machines").val(2);
+      }
+    }
+  });
+
+  $("#algorithm").change(function() {
+    if ($(this).val() === 'johnson') {
+        $("#nb_machines").val(2);
+    }
+  });
+
+  $("#solve").on("click", function() {
+    $.ajax({
+      url: "/solve",
+      dataType: "json",
+      type: "post",
+      contentType: "application/json",
+      data: JSON.stringify({
+        algorithm: $("#algorithm").val(),
+        data: $("#data").val(),
+        nb_machines: $("#nb_machines").val(),
+        nb_jobs: $("#nb_jobs").val()
+      }),
+      processData: false,
+      success: function(data, textStatus, jQxhr) {
+        Plotly.newPlot("gantt", JSON.parse(data["graph"]), {});
+        $("#sequence").text(data["opt_seq"]);
+      },
+      error: function(jQxhr, textStatus, errorThrow) {
+        console.log(textStatus);
+      }
     });
-}); 
-
-
-
-
-
-
-
-
+  });
+});
