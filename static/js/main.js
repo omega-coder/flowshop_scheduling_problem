cde64a2568c7d920ed8fe6a4d6579fd7dcc1abcb
omega-coder <fy_cherief@esi.dz>
2019-07-17 09:33:49 +0200

    Handles when nograph is specified

--- static/js/main.js
+++ static/js/main.js
@@ -46,6 +46,7 @@ $(document).ready(function() {
       });
     } else if ($("#algorithm").val() === "genetic-algorithm") {
       var data_tbs = JSON.stringify({
+        nograph: true,
         algorithm: $("#algorithm").val(),
         data: $("#data").val(),
         nb_machines: $("#nb_machines").val(),
@@ -72,11 +73,19 @@ $(document).ready(function() {
       data: data_tbs,
       processData: false,
       success: function(data, textStatus, jQxhr) {
-        Plotly.newPlot("gantt", JSON.parse(data["graph"]), {});
-        $("#sequence").text(data["opt_seq"]);
-        $("#opt_makespan").text(data["optim_makespan"]);
-        var time_str = data["t_time"].toString() + " " + data["tt"].toString();
-        $("#time").text(time_str);
+        if (data["graph"] === null) {
+          $("#sequence").text(data["opt_seq"]);
+          $("#opt_makespan").text(data["optim_makespan"]);
+          var time_str = data["t_time"].toString() + " " + data["tt"].toString();
+          $("#time").text(time_str);
+        } else {
+          Plotly.newPlot("gantt", JSON.parse(data["graph"]), {});
+          $("#sequence").text(data["opt_seq"]);
+          $("#opt_makespan").text(data["optim_makespan"]);
+          var time_str = data["t_time"].toString() + " " + data["tt"].toString();
+          $("#time").text(time_str);
+        }
+        
       },
       error: function(jQxhr, textStatus, errorThrow) {
         console.log(textStatus);
